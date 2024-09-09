e87d7766dff817a1ddbdfd06445c3d27e57ce72f
dragzter <vladimir.mujakovic@gmail.com>
2024-09-09 10:41:36 -0400

    fix img src

--- client/src/views/ReturnPolicyView.vue
+++ client/src/views/ReturnPolicyView.vue
@@ -14,7 +14,7 @@
             <div class="text-center my-5">
               <img
                   alt="logo"
-                  src="../assets/img/rpgavatarlogo.png"
+                  src="/public/img/rpgavatarlogo.png"
               />
             </div>
             <p>
